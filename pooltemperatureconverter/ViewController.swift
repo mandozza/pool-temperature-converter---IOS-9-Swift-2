bf0797958b05573592a91373daa894354e1b4eeb
Jeremy Josey <webjeremy@gmail.com>
2016-02-02 13:08:42 -0400

    Initial Commit

---
+++ pooltemperatureconverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pooltemperatureconverter
+//
+//  Created by Jeremy Josey on 2016-02-02.
+//  Copyright © 2016 Jeremy Josey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
