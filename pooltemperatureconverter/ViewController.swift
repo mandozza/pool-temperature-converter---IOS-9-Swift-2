8fb6bfa2051c622afda389c01957f4207acab8e9
Jeremy Josey <webjeremy@gmail.com>
2016-02-02 13:53:58 -0400

    First Commit
    Final Build iOS Temperature converter

--- pooltemperatureconverter/ViewController.swift
+++ pooltemperatureconverter/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var lblFahrenheit: UILabel!
+    @IBOutlet weak var lblCelcius: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    
+    @IBAction func sliderAction(sender: AnyObject) {
+        convert()
+        
+    }
+    
+    
+    func convert(){
+        
+        let fahrenheit = slider.value;
+        let celcius = ((fahrenheit - 32) / 1.8)
+        
+        updateLabels(fahrenheit,celcius: celcius)
+        
     }
 
+ 
+    func updateLabels(fahrenheit: Float, celcius: Float){
+        lblFahrenheit.text = String(format: "%4.0f Fahrenheit", fahrenheit)
+        lblCelcius.text = String(format: "%4.1f Celcius", celcius)
+        
+    }
 
 }
 
